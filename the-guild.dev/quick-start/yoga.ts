1ea2c9eaee170aab9dc0afdf4f188f794dc13280
PeterOnCode <peteroncode@gmail.com>
2023-04-11 16:09:19 +0200

    [the-guild] : Error Masking : Exposing expected errors

--- the-guild.dev/quick-start/yoga.ts
+++ the-guild.dev/quick-start/yoga.ts
@@ -1,28 +1,17 @@
-import { createSchema, createYoga } from 'graphql-yoga'
-import { fetch } from '@whatwg-node/fetch'
+import {createYoga } from 'graphql-yoga'
+import { createAppSchema } from './schema.js'
+import { createContext } from './context.js'
 
 export const yoga = createYoga(
     {
       graphiql : {
         defaultQuery : /*GraphQL*/`
-          query {
-            greeting
+          {
+            user(byId: 1) {
+              login
+            }
           }`
       },
-      schema : createSchema( {
-        typeDefs : /* GraphQL */ `
-          type Query {
-            greeting: String!
-          }
-        `,
-        resolvers : {
-          Query : {
-            greeting : async () => {
-              return await fetch( 'http://localhost:9876/greetings' ).then(
-                res => res.text()
-              )
-            }
-          }
-        }
-      } )
+      schema : createAppSchema,
+      context: createContext
     } )
