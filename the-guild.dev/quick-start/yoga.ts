4cde5c3c9c07a476ccbafaf89f33c57bfcb72417
PeterOnCode <peteroncode@gmail.com>
2023-04-12 13:36:49 +0200

    [the-guild] : Error Masking : Error codes and other extensions

--- the-guild.dev/quick-start/yoga.ts
+++ the-guild.dev/quick-start/yoga.ts
@@ -1,17 +1,16 @@
 import {createYoga } from 'graphql-yoga'
 import { createAppSchema } from './schema.js'
-import { createContext } from './context.js'
 
 export const yoga = createYoga(
     {
       graphiql : {
+        title: 'Error Masking',
         defaultQuery : /*GraphQL*/`
           {
-            user(byId: 1) {
+            user(byId: 6) {
               login
             }
           }`
       },
-      schema : createAppSchema,
-      context: createContext
+      schema : createAppSchema
     } )
