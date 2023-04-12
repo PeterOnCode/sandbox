c42721f7aed19e3e338bd402da3e02820060bc15
PeterOnCode <peteroncode@gmail.com>
2023-04-12 14:00:04 +0200

    [the-guild] : Error Masking : Customize error masking

--- the-guild.dev/quick-start/yoga.ts
+++ the-guild.dev/quick-start/yoga.ts
@@ -1,15 +1,23 @@
-import {createYoga } from 'graphql-yoga'
+import { createYoga, maskError } from 'graphql-yoga'
 import { createAppSchema } from './schema.js'
 
 export const yoga = createYoga(
     {
       graphiql : {
-        title: 'Error Masking',
+        title : 'Error Masking',
         defaultQuery : /*GraphQL*/`
           {
             user(byId:  6)
           }`
       },
       schema : createAppSchema,
-      // maskedErrors: false
+      maskedErrors : {
+        maskError( error, message, isDev ) {
+          if ( error?.extensions?.code === 'DOWNSTREAM_SERVICE_ERROR' ) {
+            return error
+          }
+
+          return maskError( error, message, isDev )
+        }
+      }
     } )
