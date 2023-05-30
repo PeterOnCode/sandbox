2bcf12f6880e1b959e0ef67378a2e44000b8f7b5
PeterOnCode <peteroncode@gmail.com>
2023-05-30 12:46:54 +0200

    [add-supabase-authentication-to-vue] :: Use constants

--- add-supabase-authentication-to-vue/src/composables/UseSupaBase.ts
+++ add-supabase-authentication-to-vue/src/composables/UseSupaBase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import constants from './constants'
 
-
-const supabaseUrl = 'https://wftwjzyqhdneelyshzke.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzc3NDM4MCwiZXhwIjoxOTQ5MzUwMzgwfQ.Z7FOfAT_jSUDPsVuWvr0hAA8Cp2ySb4_peni8r5pMjo'
+const { supabaseUrl, supabaseKey } = constants
 
 // setup client
 const supabase = createClient( supabaseUrl, supabaseKey )
