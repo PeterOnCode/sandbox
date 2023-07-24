671981431324def1e44f8467ea026cdec9cbe869
PeterOnCode <peteroncode@gmail.com>
2023-07-24 22:11:59 +0200

    startup (open-wc--ui-library) : Init

---
+++ open-wc--ui-library/src/index.ts
@@ -0,0 +1,1 @@
+export { OpenWcUiLibrary } from './OpenWcUiLibrary.js';
---
+++ open-wc--ui-library/src/open-wc--ui-library.ts
@@ -0,0 +1,3 @@
+import { OpenWcUiLibrary } from './OpenWcUiLibrary.js';
+
+window.customElements.define('open-wc--ui-library', OpenWcUiLibrary);
