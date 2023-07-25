b59d761b1aa74ed7a2c789822b050a7ed5337ccc
PeterOnCode <peteroncode@gmail.com>
2023-07-25 12:42:46 +0200

    feat (open-wc--ui-library) : Give It Some Style

--- open-wc--ui-library/src/BaseButton.ts
+++ open-wc--ui-library/src/BaseButton.ts
@@ -1,8 +1,25 @@
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 @customElement('base-button')
 export class BaseButton extends LitElement {
+  static styles = css`
+    button.base-btn {
+      color: white;
+      background: #2a63bf;
+      border: 0px transparent;
+      border-radius: 0.5rem;
+      padding: 0.5rem 1rem;
+      box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.125);
+      transition: background 250ms ease-in-out;
+      margin: 5px;
+    }
+
+    button.base-btn:hover {
+      cursor: pointer;
+      background: #204b91;
+    }
+  `;
   @property({ type: String }) text = 'Base Button';
 
   render() {
